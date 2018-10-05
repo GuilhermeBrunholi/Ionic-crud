522b4e8910241432c0f2abfb9c61c510156ad43c
Guilherme  Brunholi <gui_brunholi@hotmail.com>
2018-10-05 19:52:42 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicStorageModule } from '@ionic/storage'
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ClienteProvider } from '../providers/cliente/cliente';
+import { DatePipe } from '@angular/common';
+import { EditarPage } from '../pages/editar/editar';
 
 @NgModule({
   declarations: [
@@ -14,17 +17,21 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EditarPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    DatePipe,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ClienteProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { ClienteProvider, ClienteLista } from '../../providers/cliente/cliente';
+import { EditarPage } from '../editar/editar';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,45 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  clientes:ClienteLista[];
+
+  constructor(
+    public navCtrl: NavController,
+    private clienteProvide: ClienteProvider,
+    private toast: ToastController
+    ) {
 
   }
+
+  ionViewDidEnter() {
+    this.clienteProvide.getAll()
+    .then(results => {
+      this.clientes = results;
+    })
+  }
+
+  addCliente(){
+    this.navCtrl.push('EditarPage');
+  }
+
+  editCliente(item: ClienteLista){
+    this.navCtrl.push('EditarPage', {key:item.key, cliente:item.cliente});
+  }
+
+  removeCliente(item: ClienteLista){
+    this.clienteProvide.remove(item.key)
+    .then(() => {
+
+      let index = this.clientes.indexOf(item);
+      this.clientes.splice(index, 1);
+
+      this.toast.create({
+        message: 'Cliente Removido',
+        duration: 3000,
+        position: 'button'
+      }).present();
+    })
+  }
+  
 
 }
